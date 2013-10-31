9330b27f2baa1c59f1e5c1d0d162ba92ffcc779f
eointravers <eoint02@gmail.com>
2013-10-31 11:24:35 +0000

    Creating

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,8 +5,8 @@ from __future__ import unicode_literals
 AUTHOR = u'Belfast Science Cafe'
 SITENAME = u'Belfast Science Cafe'
 SITEURL = ''
+TIMEZONE = 'Europe/London'
 
-TIMEZONE = 'Europe/Paris'
 
 DEFAULT_LANG = u'en'
 
@@ -16,18 +16,21 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
+LINKS =  (('What should we link to?', 'http://getpelican.com/'),
           ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+          ('Jinja2', 'http://jinja.pocoo.org/'))
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Facebook', '#'),
+          ('Twitter?', '#'),)
 
 DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-#THEME = 'themes/nmnlist'
+THEME = 'themes/notmyidea'
+DISPLAY_CATEGORIES_ON_MENU = False
+INDEX_SAVE_AS = 'pages/blog.html'
+PLUGIN_PATH = '../pelican-plugins'
+PLUGINS = ['liquid_tags.img']
